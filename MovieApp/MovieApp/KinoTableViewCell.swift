b797519f41f2b5f54c4bfb9152cc422cba5d52f9
Arturka82 <artur.gedakyan1992@mail.ru>
2021-03-08 21:21:40 +0300

    правки

---
+++ MovieApp/MovieApp/KinoTableViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  KinoTableViewCell.swift
+//  KinoPoisk
+//
+//  Created by Artur Gedakyan on 05.03.2021.
+//
+
+import UIKit
+
+final class KinoTableViewCell: UITableViewCell {
+    static var id = "KinoTableViewCell"
+
+    @IBOutlet var kinoImage: UIImageView!
+    @IBOutlet var kinoNameLabel: UILabel!
+    @IBOutlet var kinoDescriptionLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    static func nib() -> UINib {
+        let inibLocal = UINib(nibName: "KinoTableViewCell", bundle: nil)
+
+        return inibLocal
+    }
+}
+
+extension KinoTableViewCell: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "KinoTableViewCell", for: indexPath)
+        return cell
+    }
+}
